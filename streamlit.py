c83b809a8049817bdb67e3b16795ad5b0fc0069e
stubbs <stubbs0x@protonmail.com>
2022-09-01 13:18:26 -0600

    added claimable rook and pie charts for balances/volume

--- streamlit.py
+++ streamlit.py
@@ -3,13 +3,19 @@ import pandas as pd
 from transpose import Transpose
 import requests
 import os
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from dotenv import load_dotenv
 
 #------------------------------------------------------#
 #Write the necessary functions
-
-
-
+def get_rook_reward():
+    url = "https://api.rook.fi/api/v1/coordinator/userClaims?user=0x6d956A6Aaca9BB7A0e4D34b6924729F856c641dE"
+    response = (requests.get(url).json())
+    earning_to_date = (response['latestCommitment']['earningsToDate'])/pow(10,18)
+    total_claimed = (response['totalClaimed'])/pow(10,18)
+    claimable = earning_to_date - total_claimed
+    return earning_to_date, total_claimed, claimable
 
 
 
@@ -67,20 +73,62 @@ st.title("Rook Multisig Testing Wallet")
 st.write("Learn More: [KIP-30](https://forum.rook.fi/t/kip-30-temporarily-empower-and-fund-a-strategy-testing-multisig/395)")
 
 
+
+rook_earned_col, rook_claimed_col, rook_claimable_col,  = st.columns(spec=3, gap="medium")
+
 st.header("Current Balances")
+
+stablecol_1, stablecol_2, stablecol_3, stablecol_4 = st.columns(spec=4, gap="small")
+
+labels = ["USDC", "USDT", "DAI", "FRAX"]
+wallet_balances = [f"{float(bal[bal['name'] == 'USD Coin']['amount']):.0f}",f"{float(bal[bal['name'] == 'Tether USD']['amount']):.0f}", f"{float(bal[bal['name'] == 'Dai Stablecoin']['amount']):.0f}", f"{float(bal[bal['name'] == 'Frax']['amount']):.0f}"]
+stablecoin_volume = [token_sent[token_sent['symbol_x']=='USDC']['token_amount_x'].sum(), token_sent[token_sent['symbol_x']=='USDT']['token_amount_x'].sum(), token_sent[token_sent['symbol_x']=='DAI']['token_amount_x'].sum(), token_sent[token_sent['symbol_x']=='FRAX']['token_amount_x'].sum()]
+# Create subplots: use 'domain' type for Pie subplot
+fig = make_subplots(rows=1, cols=2, specs=[[{'type':'domain'}, {'type':'domain'}]])
+fig.add_trace(go.Pie(labels=labels, values=wallet_balances, name="Stablecoin Balances"),
+              1, 1)
+fig.add_trace(go.Pie(labels=labels, values=stablecoin_volume, name="Trading Volume"),
+              1, 2)
+# Use `hole` to create a donut-like pie chart
+fig.update_traces(hole=.4, hoverinfo="label+percent+value")
+fig.update_layout(
+    title_text="Current Wallet Testing Wallet Balances & Volume",
+    # Add annotations in the center of the donut pies.
+    annotations=[dict(text='Balance', x=0.2, y=0.50, font_size=25, showarrow=False),
+                 dict(text='Volume', x=0.80, y=0.5, font_size=25, showarrow=False)],
+    autosize = True,
+    width=900,
+    height=650,)
+st.plotly_chart(fig,use_container_width=True)
+
+
+stable_vol_1, stable_vol_2, stable_vol_3, stable_vol_4 = st.columns(spec=4, gap="small")
+rook_reward=get_rook_reward()
+
+
+
+
 # with st.expander("Current Balances"):
 
-stablecol_1, stablecol_2, stablecol_3, stablecol_4 = st.columns(spec=4, gap="medium")
+
+
+
+
 trading_details_expander = st.expander("Trading Details")
 # st.line_chart()
 # st.table(token_sent[(token_sent['symbol_x']=='USDC') | (token_sent['symbol_y']=='USDC')][['timestamp_x', 'path', 'token_amount_x']].sort_values(by='timestamp_x',ascending=False))
 token_sent['cumsum']=token_sent.groupby(['timestamp_x', 'path'])['token_amount_x'].cumsum()
 st.dataframe(token_sent[['timestamp_x', 'path', 'token_amount_x', 'cumsum']])
+with rook_earned_col:
+    st.metric(label="Total $ROOK Earned", value=f"{rook_reward[0]:.2f} $ROOK")
+with rook_claimed_col:
+    st.metric(label="Total $ROOK Claimed", value=f"{rook_reward[1]:.2f} $ROOK")
+with rook_claimable_col:
+    st.metric(label="Claimable $ROOK", value=f"{rook_reward[2]:.2f} $ROOK")
+
 with stablecol_1:
     st.subheader("USDC")
-    total_usdc_volume = token_sent[token_sent['symbol_x']=='USDC']['token_amount_x'].sum()
-
-    st.metric(label="USDC Volume", value=f"${total_usdc_volume:,.0f}")
+   
     token_sent[['path','tx_hash']].groupby('path').count()
     
     
@@ -89,53 +137,53 @@ with stablecol_1:
     change = (usdc_balance-150000)/150000
 
     st.metric(label="USDC Balance", value = f"${usdc_balance:,.2f}", delta =f"{change:.2%}")
-    st.dataframe(token_sent[(token_sent['symbol_x']=='USDC') | (token_sent['symbol_y']=='USDC')][['timestamp_x', 'path', 'token_amount_x']].sort_values(by='timestamp_x', ascending=False))
+
+
     # st.table((token_sent[token_sent['symbol_x']=='USDC') | ][['timestamp_x','path', 'token_amount_x']].sort_values('timestamp_x', ascending=False))
+with stable_vol_1:
+    total_usdc_volume = token_sent[token_sent['symbol_x']=='USDC']['token_amount_x'].sum()
+    st.metric(label="USDC Volume", value=f"${total_usdc_volume:,.0f}")
+    st.dataframe(token_sent[(token_sent['symbol_x']=='USDC') | (token_sent['symbol_y']=='USDC')][['timestamp_x', 'path', 'token_amount_x']].sort_values(by='timestamp_x', ascending=False), )
+
 with stablecol_2:
     st.subheader("DAI")
-    total_dai_volume = token_sent[token_sent['symbol_x']=='DAI']['token_amount_x'].sum()
-    st.metric(label="DAI Volume", value=f"${total_dai_volume:,.0f}")
-
-
     dai_balance = float(bal[bal['name'] == 'Dai Stablecoin']['amount'])
     change = (dai_balance-150000)/150000
-
     st.metric(label="DAI", value = f"${dai_balance:,.2f}", delta =f"{change:.2%}")
-    st.dataframe(token_sent[(token_sent['symbol_x']=='DAI') | (token_sent['symbol_y']=='DAI')][['timestamp_x', 'path', 'token_amount_x']].sort_values(by='timestamp_x', ascending=False))
 
     # st.table(token_sent[token_sent['symbol_x']=='DAI'][['timestamp_x','path', 'token_amount_x']].sort_values('timestamp_x', ascending=False))
     # with st.expander("Recent Trades"):
     # st.write("# Trades")
+with stable_vol_2:
+    total_dai_volume = token_sent[token_sent['symbol_x']=='DAI']['token_amount_x'].sum()
+    st.metric(label="DAI Volume", value=f"${total_dai_volume:,.0f}")
+    st.dataframe(token_sent[(token_sent['symbol_x']=='DAI') | (token_sent['symbol_y']=='DAI')][['timestamp_x', 'path', 'token_amount_x']].sort_values(by='timestamp_x', ascending=False))
+
 with stablecol_3:
     st.subheader("USDT")
-
-    total_usdt_volume = token_sent[token_sent['symbol_x']=='USDT']['token_amount_x'].sum()
-    st.metric(label="USDT Volume", value=f"${total_usdt_volume:,.0f}")
-
 
     usdt_balance = float(bal[bal['name'] == 'Tether USD']['amount'])
     change = (usdt_balance-150000)/150000
     st.metric(label="USDT", value = f"${usdt_balance:,.2f}", delta =f"{change:.2%}")
+
+with stable_vol_3:
+    total_usdt_volume = token_sent[token_sent['symbol_x']=='USDT']['token_amount_x'].sum()
+    st.metric(label="USDT Volume", value=f"${total_usdt_volume:,.0f}")
     st.dataframe(token_sent[(token_sent['symbol_x']=='USDT') | (token_sent['symbol_y']=='USDT')][['timestamp_x', 'path', 'token_amount_x']].sort_values(by='timestamp_x', ascending=False))
 
-    # st.table(token_sent[token_sent['symbol_x']=='USDT'][['timestamp_x','path', 'token_amount_x']].sort_values('timestamp_x', ascending=False))
-    # with st.expander("Recent Trades"):
-    # st.write("# Trades")
 with stablecol_4:
     st.subheader("FRAX")
-
-    total_frax_volume = token_sent[token_sent['symbol_x']=='FRAX']['token_amount_x'].sum()
-    st.metric(label="FRAX Volume", value=f"${total_frax_volume:,.0f}")
-    
- 
     frax_balance = float(bal[bal['name'] == 'Frax']['amount'])
     change = (frax_balance-150000)/150000
     st.metric(label="FRAX", value = f"${frax_balance:,.0f}", delta =f"{change:.2%}")
+
+with stable_vol_4:
+    total_frax_volume = token_sent[token_sent['symbol_x']=='FRAX']['token_amount_x'].sum()
+    st.metric(label="FRAX Volume", value=f"${total_frax_volume:,.0f}")
     st.dataframe(token_sent[(token_sent['symbol_x']=='FRAX') | (token_sent['symbol_y']=='FRAX')][['timestamp_x', 'path', 'token_amount_x']].sort_values(by='timestamp_x', ascending=False))
 
-    # st.table(token_sent[token_sent['symbol_x']=='FRAX'][['timestamp_x','path', 'token_amount_x']].sort_values('timestamp_x', ascending=False))
-    # with st.expander("Recent Trades"):
-    # st.write("# Trades")
+
+
 with trading_details_expander:
     cum_sum = token_sent.groupby(['timestamp_x', 'path'])['token_amount_x'].cumsum()
     st.dataframe(cum_sum)
